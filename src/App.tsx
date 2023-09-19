d9fd04c713805240656e36654d0b4811872ce9d3
khaja-samdani <khaja.samdani12@gmail.com>
2023-09-19 16:54:11 +0530

    created a simple todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputTextFiled from './components/InputTextFiled';
 import { modal } from './components/modal';
 import TodoList from './components/TodoList';
 
-const App:React.FC = () => {
-  const [todo,setTodo]=useState<string>("")
-  const [todos,setTodos]=useState<modal[]>([])
-const handleAdd=(e:React.FormEvent)=>{
-  e.preventDefault()
-  setTodos([...todos,{id:Date.now(),todo,isDone:false}])
-  setTodo("")
-}
-console.log(todos)
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("")
+  const [todos, setTodos] = useState<modal[]>([])
+  const handleAdd = (e: React.FormEvent) => {
+    if (todo) {
+      e.preventDefault()
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }])
+      setTodo("")
+    } else {
+      alert('please enter text')
+    }
+  }
+  console.log(todos)
   return (
     <div className='App'>
       <div className='header'>
-      TASKIFY
+        TASKIFY
       </div>
       <InputTextFiled todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
       <TodoList todos={todos} setTodos={setTodos} />
